255d61219e6127484243fa6d3ad0afc09fc80d96
GaoVinfly <gao778751367@gmail.com>
2025-10-28 00:02:04 +0800

    test01

---
+++ src/main/java/com/example/testgit/TestController.java
@@ -0,0 +1,10 @@
+package com.example.testgit;
+
+/**
+ * @author gavin
+ * @version 1.0
+ * @date 2025/10/27 23:56
+ */
+public class TestController {
+    public static final String WORD_TEST = "WORLDTEST";
+}
